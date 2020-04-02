91d6003753af3a952c5a967a921b997c7d624ee5
amanjain4103 <amanjain4103@gmail.com>
2020-04-02 19:04:27 +0530

    adding whole project on git

---
+++ server.js
@@ -0,0 +1,62 @@
+const express = require('express');
+const bodyParser = require('body-parser');
+const cors = require('cors');
+const knex = require('knex');
+const bcrypt = require('bcrypt-nodejs');
+const register = require('./controllers/register');
+const signin = require('./controllers/signin');
+const profile = require('./controllers/profile');
+const image = require('./controllers/image');
+
+const db = knex({
+    client:'pg',
+    connection:{
+        host:'127.0.0.1',
+        user:'postgres',
+        password:'system',
+        database:'imagerecog'
+    }
+});
+
+
+
+
+const app = express();
+
+app.use(bodyParser.urlencoded({extended:false}));
+app.use(bodyParser.json());
+app.use(cors());
+
+app.get('/',(req,res)=>{
+    res.send(database.users);
+})
+
+
+app.post('/signin',(req,res) => {  signin.handleSignin(req,res,db,bcrypt) })
+
+app.post('/register', (req,res) => { register.handleRegister(req,res,db,bcrypt) }  )
+
+app.get('/profile/:id',(req,res) => {  msWriteProfilerMark.handleProfileGet(req,res,db) })
+
+app.put('/image', (req,res) => { Image.handleImage(req,res,db) })
+
+
+
+
+
+
+app.listen(3000,()=>{
+    console.log("app is running");
+})
+
+
+
+
+/*
+/ --> this is working 
+/signin --> POST = success/fail
+/register --> POST =  user
+/profile/:userid --> GET user
+/image --> PUT --> update user info
+
+*/
